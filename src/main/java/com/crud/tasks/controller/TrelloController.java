96f7d7dc4b4b2cd35a808327b5429fd694712103
Kreatorzy Wnętrz <KREATORZYWNETRZ@MacBook-Pro.local>
2022-07-22 09:22:45 +0200

    22.2 Code arrangement

---
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -0,0 +1,37 @@
+package com.crud.tasks.controller;
+
+import com.crud.tasks.domain.TrelloBoardDto;
+import com.crud.tasks.trello.client.TrelloClient;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("v1/trello")
+@RequiredArgsConstructor
+@CrossOrigin("*")
+public class TrelloController {
+
+    private final TrelloClient trelloClient;
+
+    @GetMapping("boards")
+    public void getTrelloBoards() {
+
+        List<TrelloBoardDto> trelloBoards = trelloClient.getTrelloBoards();
+
+        List<TrelloBoardDto> filteredBoards = trelloBoards.stream()
+                        .filter(trelloBoardDto -> Objects.nonNull(trelloBoardDto.getId()) && Objects.nonNull(trelloBoardDto.getName()))
+                        .filter(trelloBoardDto -> trelloBoardDto.getName().contains("Kodilla"))
+                        .collect(Collectors.toList());
+        trelloBoards.forEach(trelloBoardDto -> {
+            System.out.println(trelloBoardDto.getId() + " " + trelloBoardDto.getName());
+        });
+    }
+}
