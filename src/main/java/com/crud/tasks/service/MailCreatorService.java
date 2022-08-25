a623139c665e42341c5b4f7c3cac2dd3cef4fea6
Kreatorzy Wnętrz <KREATORZYWNETRZ@MacBook-Pro.local>
2022-08-25 21:15:39 +0200

    Fix api test1

--- src/main/java/com/crud/tasks/service/MailCreatorService.java
+++ src/main/java/com/crud/tasks/service/MailCreatorService.java
@@ -8,6 +8,9 @@ import org.springframework.stereotype.Service;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class MailCreatorService {
 
@@ -22,6 +25,12 @@ public class MailCreatorService {
     private TemplateEngine templateEngine;
 
     public String buildTrelloCardEmail(String message) {
+
+        List<String> functionality = new ArrayList<>();
+        functionality.add("You can manage your tasks");
+        functionality.add("Provides connection with Trello Account");
+        functionality.add("Application allows sending tasks to Trello");
+
         Context context = new Context();
         context.setVariable("message", message);
         context.setVariable("tasks_url", "http://grzeszk.github.io");
@@ -33,6 +42,9 @@ public class MailCreatorService {
         context.setVariable("company_phone", companyDetails.getCompanyPhone());
         context.setVariable("goodbye_message", "Yours sincerely task CRUD api team");
         context.setVariable("preview_message", "Trello board update info");
+        context.setVariable("is_friend", false);
+        context.setVariable("admin_config", adminConfig);
+        context.setVariable("application_functionality", functionality);
         return templateEngine.process("mail/created-trello-card-mail", context);
     }
 }
